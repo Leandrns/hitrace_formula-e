421bd397ca8f152848dec36c5c38a77fea630879
Leandro Souza <leandro2005souza@gmail.com>
2024-10-06 11:40:09 -0300

    .

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from "react"
 import './style.css'
 
 export const Chat = ({ socket }) => {
-    const bottomRef = useRef()
     const messageRef = useRef()
     const [messageList, setMessageList] = useState([])
 
