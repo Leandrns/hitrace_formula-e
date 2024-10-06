000203c70f6f37da59d5c907c307e939a74c1a74
Leandro Souza <leandro2005souza@gmail.com>
2024-10-06 15:21:49 -0300

    criando json com os dados

--- src/components/TelaPrincipal/index.jsx
+++ src/components/TelaPrincipal/index.jsx
@@ -5,136 +5,15 @@ import { Escolhas } from '../Escolhas/index.jsx'
 import { useState } from 'react'
 import { ListaOpcoes } from '../ListaOpcoes/index.jsx'
 import { AoVivo } from '../AoVivo/index.jsx';
+import dados from '../../json/dados.json'
 
-const pilotos1 = [
-    {
-        "id": 0,
-        "tipo": "piloto1",
-        "nome": "Nick DE VRIES",
-        "equipe": "Mahindra Racing",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/8bd68290-c0ad-43b6-a1ce-ee162b7d8914.png',
-        "preco": 15,
-        "desempenho": 12
-    },
-    {
-        "id": 1,
-        "tipo": "piloto1",
-        "nome": "Mitch EVANS",
-        "equipe": "Jaguar TCS Racing",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/2543b91c-2543-4835-b9fc-17b5fbc79cb1.png',
-        "preco": 18,
-        "desempenho": 16
-    },
-    {
-        "id": 2,
-        "tipo": "piloto1",
-        "nome": "Antonio Felix DA COSTA",
-        "equipe": "Porsche",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/fcc99980-3e5d-4a96-856a-8773641ceebe.png',
-        "preco": 17,
-        "desempenho": 15
-    }
-]
-
-const pilotos2 = [
-    {
-        "id": 0,
-        "tipo": "piloto2",
-        "nome": "Nick DE VRIES",
-        "equipe": "Mahindra Racing",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/8bd68290-c0ad-43b6-a1ce-ee162b7d8914.png',
-        "preco": 15,
-        "desempenho": 12
-    },
-    {
-        "id": 1,
-        "tipo": "piloto2",
-        "nome": "Mitch EVANS",
-        "equipe": "Jaguar TCS Racing",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/2543b91c-2543-4835-b9fc-17b5fbc79cb1.png',
-        "preco": 18,
-        "desempenho": 16
-    },
-    {
-        "id": 2,
-        "tipo": "piloto2",
-        "nome": "Antonio Felix DA COSTA",
-        "equipe": "Porsche",
-        "img": 'https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/fcc99980-3e5d-4a96-856a-8773641ceebe.png',
-        "preco": 17,
-        "desempenho": 15
-    }
-]
-
-const tecnicos = [
-    {
-        "id": 0,
-        "tipo": "tecnico",
-        "nome": "Florian MODLINGER",
-        "equipe": "Porsche",
-        "img": '/Florian_porsche.png',
-        "preco": 15,
-        "desempenho": 12
-    },
-    {
-        "id": 1,
-        "tipo": "tecnico",
-        "nome": "Frederic Bertrand",
-        "equipe": "Mahindra Racing",
-        "img": '/bertrand_mahindra.png',
-        "preco": 14,
-        "desempenho": 11
-    }
-]
-
-const equipes = [
-    {
-        "id": 0,
-        "tipo": "equipe",
-        "nome": "Mahindra Racing",
-        "equipe": "",
-        "img": 'https://static-files.formula-e.pulselive.com/cars/84467676-4d5d-4c97-ae07-0b7520bb95ea/fa97d2e7-02ca-4983-a930-4fdaa245a852.png',
-        "preco": 15,
-        "desempenho": 14
-    },
-    {
-        "id": 1,
-        "tipo": "equipe",
-        "nome": "Jaguar TCS Racing",
-        "equipe": "",
-        "img": 'https://static-files.formula-e.pulselive.com/cars/84467676-4d5d-4c97-ae07-0b7520bb95ea/05dab754-2899-411b-9c4e-72311a36cc9c.png',
-        "preco": 14,
-        "desempenho": 13
-    }
-]
-
-const motores = [
-    {
-        "id": 0,
-        "tipo": "motor",
-        "nome": "Jaguar I-Type 6",
-        "equipe": "",
-        "img": 'https://upload.wikimedia.org/wikipedia/pt/8/86/Logotipo_da_Jaguar_2012.png',
-        "preco": 14,
-        "desempenho": 12
-    },
-    {
-        "id": 1,
-        "tipo": "motor",
-        "nome": "Mahindra M9Electro",
-        "equipe": "",
-        "img": 'https://cdn.worldvectorlogo.com/logos/mahindra-mahindra-logo.svg',
-        "preco": 15,
-        "desempenho": 13
-    }
-]
 
 export const TelaPrincipal = ({ telaAtiva }) => {
-    localStorage.setItem('pilotos1', JSON.stringify(pilotos1));
-    localStorage.setItem('pilotos2', JSON.stringify(pilotos2));
-    localStorage.setItem('tecnicos', JSON.stringify(tecnicos));
-    localStorage.setItem('equipes', JSON.stringify(equipes));
-    localStorage.setItem('motores', JSON.stringify(motores));
+    localStorage.setItem('pilotos1', JSON.stringify(dados.pilotos1));
+    localStorage.setItem('pilotos2', JSON.stringify(dados.pilotos2));
+    localStorage.setItem('tecnicos', JSON.stringify(dados.tecnicos));
+    localStorage.setItem('equipes', JSON.stringify(dados.equipes));
+    localStorage.setItem('motores', JSON.stringify(dados.motores));
 
     let piloto1 = JSON.parse(localStorage.getItem('piloto1')) || false;
     let piloto2 = JSON.parse(localStorage.getItem('piloto2')) || false;
