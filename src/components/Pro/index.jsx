58bbf3a32fd49c2954ca2ebc0ae720e64b14ed7d
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-16 18:39:25 -0300

    l

--- src/components/Pro/index.jsx
+++ src/components/Pro/index.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components"
+import proHeader from '../../images/proHeader.png'
+
+export const Titulo = styled.h2`
+    width: 100%;
+    padding: 30px 0;
+    color: ${props => props.cor || '#000'};
+    font-size: ${props => props.tamanhoFonte || '18px;'};
+    text-align: ${props => props.alinhamento || 'center'};
+    margin: 0;
+`
+
+export const Centralizacao = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+export const Topo = styled.img`
+
+`
+
 export const PRO = (props) => {
     return(
-        <div></div>
+        <div>
+            <Titulo cor="#ebc417" tamanhoFonte="60px">HitRace PRO</Titulo>
+            <Centralizacao><Topo> src={proHeader} alt="" className="img" </Topo></Centralizacao>
+        </div>
     )
 }
