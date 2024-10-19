d827000156d4e807bf3d8da3f2e9bd7e98ae8139
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-18 21:55:27 -0300

    init_pro

--- src/components/Pro/index.jsx
+++ src/components/Pro/index.jsx
@@ -1,30 +1,71 @@
 import styled from "styled-components"
-import proHeader from '../../images/proHeader.png'
+import pro from '../../images/pro.svg'
 
-export const Titulo = styled.h2`
+export const PaginaPro = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Titulo = styled.div`
     width: 100%;
     padding: 30px 0;
-    color: ${props => props.cor || '#000'};
-    font-size: ${props => props.tamanhoFonte || '18px;'};
     text-align: ${props => props.alinhamento || 'center'};
     margin: 0;
+
+    img{
+        height: 150px;
+    }
 `
 
 export const Centralizacao = styled.div`
-    width: 100%;
+    width: 800px;
     display: flex;
     justify-content: center;
+    background-image: url(${() => require('../../images/proHeader.png')});
+    background-size: cover;
+    background-position: center;
+    height: 500px;
+    border-radius: 100px;
+
+    button{
+    position: absolute;
+    height: 50px;
+    width: 200px;
+    background: linear-gradient(135deg, #ebc417, #ad8e00);
+    color: white;
+
+    font-size: 18px;
+    font-weight: bold;
+    border: none;
+    border-radius: 50px;
+    box-shadow: 0 8px 15px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: all 0.3s ease;
+    outline: none;
+    font-family: 'Mina';
+    bottom: 155px;
+
+  &:hover {
+    background: linear-gradient(135deg, #ad8e00, #ebc417);
+    box-shadow: 0 15px 20px rgba(0, 0, 0, 0.2);
+    transform: translateY(-3px);
+  }
+
+  &:active {
+    transform: translateY(1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  }
+    }
 `
 
-export const Topo = styled.img`
-
-`
 
 export const PRO = (props) => {
     return(
-        <div>
-            <Titulo cor="#ebc417" tamanhoFonte="60px">HitRace PRO</Titulo>
-            <Centralizacao><Topo> src={proHeader} alt="" className="img" </Topo></Centralizacao>
-        </div>
+        <PaginaPro>
+            <Titulo><img src={pro} alt="HitRace PRO"/></Titulo>
+            <Centralizacao><button>Inscreva-se</button></Centralizacao>
+        </PaginaPro>
     )
 }
