853700f0a934d7fcb31c60ad35bc51fcb45d379f
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-22 17:41:45 -0300

    table

--- src/components/Pro/index.jsx
+++ src/components/Pro/index.jsx
@@ -7,6 +7,7 @@ import upgrade from '../../images/upgrade_card.png'
 import ads from '../../images/ads_card.png'
 import live from '../../images/live_card.png'
 import rank from '../../images/rank_card.png'
+import basic from '../../images/pro_basic.svg'
 
 export const PaginaPro = styled.div`
     display: flex;
@@ -112,10 +113,11 @@ export const Opcoes = styled.section`
     color: white;
     padding: 30px 50px 0px 50px;
     
+    
     img{
         width: 100%;
         height: 50px;
-        margin-left: 13px;
+        
     }
 
     h3{
@@ -183,23 +185,28 @@ export const CardsPro = styled.div`
     flex-direction: row;
     justify-content: center;
     flex-wrap: wrap;
+    margin: 0 17px;
     gap: 50px;
     
     
     img{
-        padding: 10px 0;
+        padding: 20px 0;
+        height: 50%;
+        
     }
 
     div.cartao{
         background-color: #CFCFCF;
         height: 400px;
-        width: 300px;
+        width: 317px;
         border-radius: 50px;
         transition: all 0.3s ease;
-        
+        text-align: center;
+        padding: 0 10px;
+
         &:hover{
             box-shadow: 0px 0px 20px #636363;
-            scale: 1.1;
+            scale: 1.05;
         }
     }
 
@@ -215,6 +222,79 @@ export const CardsPro = styled.div`
     div.textos p{
         font-size: 20px;
         padding: 0 10px;
+    }
+`
+
+export const Comparacao = styled.table`
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    border-radius: 20px;
+    padding: 30px 30px 15px 30px;
+    width: 900px;
+
+
+    img{
+        width: 300px;
+        height: 50px;
+    }
+    img.basic{
+        margin-top: 12px;
+    }
+    img.imp{
+        position: relative;
+        top: -17px;
+    }
+    th{
+        border: 0;
+        border-bottom: solid 2px #0000003b;
+        padding: 10px 10px 0 10px;
+        
+    }
+    td{
+        border: 0;
+        height: 50px;
+        border-bottom: solid 2px #0000003b;
+        padding: 10px;
+    }
+    td i{
+        font-size: 28px;
+        background-color: transparent;
+        width: 26px;
+        height: 26px;
+    }
+    i.negate{
+        color: #636363;
+        margin-top: 1px;
+    }
+    td.no{
+        border-bottom: 0;
+    }
+    td:last-child{
+        border-right: 2px solid #ebc417;
+        border-left: 2px solid #ebc417;
+    }
+    td.no:last-child{
+        border-bottom: 2px solid #ebc417;
+        border-end-start-radius: 20px;
+    }
+    th:last-child{
+        border-right: 2px solid #ebc417;
+        border-left: 2px solid #ebc417;
+        border-top: 2px solid #ebc417;
+        border-top-right-radius: 20px;
+        border-top-left-radius: 20px;
+    }
+    th.fl{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h3.popular{
+        position: relative;
+        top: -24px;
+        background: linear-gradient(90deg, #ad8e00, #ebc417);
+        width: 160px;
+        border-radius: 15px;
     }
 `
 
@@ -313,6 +393,56 @@ export const PRO = (props) => {
                         </div>
                     </div>
                 </CardsPro>
+                <h1>Escolha o plano mais adequado para você.</h1>
+                <Comparacao>
+                    <thead>
+                        <th><h3>Preços e Funcionalidades</h3></th>
+                        <th><img src={basic} alt="Pro Basico" className="basic" /></th>
+                        <th className="fl"><h3 className="popular">MAIS POPULAR</h3><img className="imp" src={pro_branco} alt="Pro" /></th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Preço</td>
+                            <td><h4>R$ 8,90 / mês</h4></td>
+                            <td><h4>R$ 24,99 / mês</h4></td>
+                        </tr>
+                        <tr>
+                            <td>Benefícios em todas as categorias</td>
+                            <td><h4><i class="fa-solid fa-x negate"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td>Transmissões Ao Vivo</td>
+                            <td><h4><i class="fa-solid fa-x negate"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td>Prêmios Exclusivos</td>
+                            <td><h4><i class="fa-solid fa-x negate"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td>Acesso ao Bot</td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td>Estatísticas Exclusivas</td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td>Upgrades Semanais</td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                            <td><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                        <tr>
+                            <td className="no">Sem Anúncios</td>
+                            <td className="no"><h4><i class="fa-solid fa-check"></i></h4></td>
+                            <td className="no"><h4><i class="fa-solid fa-check"></i></h4></td>
+                        </tr>
+                    </tbody>
+                </Comparacao>
         </PaginaPro>
     )
 }
